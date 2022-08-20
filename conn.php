d726c8b7050cb0c56add4d7e4d7e3bbbb2cb3efd
michson <41102062+michson@users.noreply.github.com>
2022-08-20 08:20:54 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,6 +3,4 @@ $conn1= mysqli_connect('localhost','root','','student_management');
 $conn2= mysqli_connect('localhost','root','','student_management');
 $conn3= mysqli_connect('localhost','root','','student_management');
 // $conn= mysqli_connect('localhost','root','','states');
-
-
 ?>
